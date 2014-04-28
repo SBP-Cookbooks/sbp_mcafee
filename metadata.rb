931fb75ad0f9b619778f4eb3776f6e51c9467081
Sander van Harmelen <svanharmelen@schubergphilis.com>
2014-04-28 20:39:39 +0200

    Updated the dependency to prevent ruby zip errors
    The windows cookbook has a fix for that merged in v1.11.0, so updated
    the dependency to a pretty recent version to make sure all works as
    expected.

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'svanharmelen@schubergphils.com'
 license          'Apache 2.0'
 description      'Installs/Configures McAfee'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 supports  	 'windows'
 
-depends   	 'windows', ">= 1.2.8"
+depends   	 'windows', ">= 1.30.0"
