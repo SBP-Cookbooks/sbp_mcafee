22273c053e20191882357d2c323112cbd1e8cfc5
Sander Botman <sbotman@schubergphilis.com>
2015-02-02 18:30:44 +0100

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,6 +20,7 @@
 default['mcafee']['agent']['package_name']      = 'McAfee Agent'
 default['mcafee']['agent']['url']               = ''
 default['mcafee']['agent']['checksum']          = ''
+default['mcafee']['agent']['options']           = '/Install=Updater /Silent'
 default['mcafee']['virusscan']['package_name']  = 'McAfee VirusScan Enterprise'
 default['mcafee']['virusscan']['url']           = ''
 default['mcafee']['virusscan']['checksum']      = ''
